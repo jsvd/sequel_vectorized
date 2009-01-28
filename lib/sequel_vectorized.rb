e59fd5c71a6dacb258a39a3f23ec0786a4bdc366
João Duarte <jsvd@jsvd-laptop.(none)>
2009-01-28 18:47:23 +0000

    Removed Sequel::Plugins::Vectorized

--- lib/sequel_vectorized.rb
+++ lib/sequel_vectorized.rb
@@ -1,35 +1,28 @@
 require 'narray'
 
-module Sequel
-  module Plugins
-    module Vectorized
+class Sequel::Dataset
+  def vectorize 
 
-      class Sequel::Dataset
-        def vectorize 
+    result = {}
 
-          result = {}
+    # transform dataset to hash of arrays
+    map {|row| row.each{|att,value| (result[att] ||= []) << value}}
 
-          # transform dataset to hash of arrays
-          map {|row| row.each{|att,value| (result[att] ||= []) << value}}
+    # transform numeric and boolean arrays to narrays
+    result.each do |k,v|
 
-          # transform numeric and boolean arrays to narrays
-          result.each do |k,v|
+      first = v.first
 
-            first = v.first
+      if first.kind_of?(Numeric) then
 
-            if first.kind_of?(Numeric) then
+        v[0] = first.to_f # so NArray is always of type float
+        result[k] = NArray.to_na v
 
-              v[0] = first.to_f # so NArray is always of type float
-              result[k] = NArray.to_na v
+      elsif first == true || first == false then
 
-            elsif first == true || first == false then
+        result[k] = NArray.float(v.size)
+        result[k][] = v.map {|i| (i == true) ? 1 : 0 }
 
-              result[k] = NArray.float(v.size)
-              result[k][] = v.map {|i| (i == true) ? 1 : 0 }
-
-            end
-          end
-        end
       end
     end
   end
--- sequel_vectorized.gemspec
+++ sequel_vectorized.gemspec
@@ -1,6 +1,6 @@
 spec = Gem::Specification.new do |s|
 	s.name              = "sequel_vectorized"
-	s.version           = "0.0.2"
+	s.version           = "0.0.3"
 	s.platform          = Gem::Platform::RUBY
 	s.has_rdoc          = false
 	s.summary           = ""
