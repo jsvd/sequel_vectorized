6af09ea6ae9f4b9d41344a8f132377a6b8a16a57
João Duarte <jsvduarte@gmail.com>
2009-06-04 14:25:07 +0100

    Added original data and raw mask to data array

--- lib/sequel_vectorized.rb
+++ lib/sequel_vectorized.rb
@@ -47,25 +47,40 @@ class Sequel::Dataset
 
     new_size = (range.last - range.first)/step.to_f
     raw_axis = data[axis_col]
+    data["__#{axis_col}".to_sym] = raw_axis
 
     interp = GSL::Interp.alloc("linear", raw_axis.size) if interpolate
 
     data[axis_col] = NArray.float(new_size).indgen!(range.first,step)
-    
-    data.each do |k,v|
 
-      next if k == axis_col 
+    data[:__raw_mask] = NArray.byte(new_size)
+    data[:__raw_mask][(raw_axis - range.first)/step.to_f] = 1
+    
+    data.keys.each do |k|
+
+      next if (k == axis_col || k == "__#{axis_col}".to_sym)
+
+      v = data[k]
 
       # if first is a float, vector is a NArray and will be interpolated
       if v[0].is_a? Float then
+        data["__#{k}".to_sym] = v #backup data in :__key
         if interpolate
-          data[k] = interp.init(raw_axis, v).eval(raw_axis, v, data[axis_col])
+          # problem with GSL lack of NArray support on compilation
+          raw_axis_vector = GSL::Vector[raw_axis.to_a]
+          v_vector = GSL::Vector[v.to_a]
+          data_vector = GSL::Vector[data[axis_col].to_a]
+
+          data[k] = interp.init(raw_axis_vector, v_vector)
+          data[k] = NArray.to_na data[k].eval(raw_axis_vector, v_vector, data_vector).to_a
         else
           data[k] = NArray.float(new_size)
           data[k][(raw_axis - range.first)/step.to_f] = v
         end
       end
     end
+
+    data
     
   end
 
